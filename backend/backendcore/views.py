3f297aebbe165a8cdd364aacee4096e73824f433
enzonjagi <amosndungo@gmail.com>
2023-04-10 12:47:02 +0300

    A skeleton update for the Django backend

--- backend/backendcore/views.py
+++ backend/backendcore/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def hello_django(request):
+    """A Simple Hello view"""
+
+    return HttpResponse("Greetings Visitor, welcome to Area ya Mzito!")
